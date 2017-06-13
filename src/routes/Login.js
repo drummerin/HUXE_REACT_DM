472f0f23789207a7d3b97a03b652170b99cce569
Susanne <>
2017-06-13 20:40:58 +0200

    signup and login functionality

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {
+    Link,
+} from 'react-router-dom';
+import * as firebase from 'firebase';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -8,10 +12,11 @@ const styles = {
     width: 100,
     marginLeft: 'auto',
     marginRight: 'auto',
+    marginTop: 15,
   },
   headline: {
     textAlign: 'center',
-    color: '#2f6fff',
+    color: '#9E9E9E',
   },
   container: {
     padding: 10,
@@ -22,40 +27,104 @@ const styles = {
     margin: 'auto',
   },
   inline: {
-    margin: '15px auto',
+    margin: '10px auto',
     padding: 10,
     width: 256,
+  },
+  buttonsDiv: {
+    width: 180,
+    margin: ' 15px auto',
   },
 };
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        email: '',
+        password: '',
+      },
+      loginError: true,
+    };
+  }
+
+  static propTypes = {
+    history: React.PropTypes.object,
+  };
+
+  handleChange = (event, newValue) => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [event.target.id]: newValue,
+      },
+    });
+
+    if (this.state.user.email.length > 4 && this.state.user.password.length > 4) {
+      this.setState({
+        loginError: false,
+      });
+    }
+  };
+
+  login() {
+    firebase.auth().signInWithEmailAndPassword(this.state.user.email,
+        this.state.user.password).then(() => {
+          // this.loginError = false;
+          this.props.history.push('/projects');
+        }).catch((error) => {
+            // Handle Errors here.
+          const errorCode = error.code;
+          // const errorMessage = error.message;
+
+          if (errorCode === 'auth/wrong-password' || errorCode === 'auth/invalid-email') {
+            // this.loginError = true;
+            // this.errorMsg = 'Email oder Passwort falsch!';
+          } else {
+            // this.loginError = true;
+            // this.errorMsg = errorMessage;
+          }
+          console.log(error);
+        });
+  }
 
   render() {
     return <div>
             <Paper style={styles.container}>
                 <div style={ styles.logo }>
-                    <img src="assets/HuxeLogo.png" width="100"/>
-                    <h1 style={styles.headline}>Login</h1>
+                    <img src="assets/logo.png" width="100" style={{ backgroundColor: '#9E9E9E' }}/>
+                    <h1 style={ styles.headline }>Login</h1>
                 </div>
 
                 <div style={styles.inline}>
                     <TextField floatingLabelText="Email"
                                style={{ marginLeft: 'auto', marginRight: 'auto' }}
+                               id="email"
+                               onChange={this.handleChange}
                     />
                 </div>
 
                 <div style={styles.inline}>
                     <TextField floatingLabelText="Password"
+                               id="password"
+                               onChange={this.handleChange}
                            type="password"
                     />
                 </div>
 
-                <RaisedButton label="login"
-                              primary={true}
-                              />
+                <div style={ styles.buttonsDiv } >
+                    <RaisedButton label="login"
+                                  primary={true}
+                                  onTouchTap={() => { this.login(); }}
+                                  disabled={this.state.loginError}
+                                  />
 
-                <RaisedButton label="signup"
-                />
+                    <Link to="/signup">
+                        <RaisedButton label="signup"
+                        />
+                    </Link>
+                </div>
             </Paper>
         </div>;
   }
--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import {
+    Link,
+} from 'react-router-dom';
+import * as firebase from 'firebase';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+
+let nameError = true;
+let emailError = true;
+let passwordError = true;
+const emailRE = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const styles = {
   logo: {
     width: 100,
     marginLeft: 'auto',
     marginRight: 'auto',
+    marginTop: 15,
   },
   headline: {
     textAlign: 'center',
-    color: '#2f6fff',
+    color: '#9E9E9E',
   },
   container: {
     padding: 10,
@@ -22,7 +32,7 @@ const styles = {
     margin: 'auto',
   },
   inline: {
-    margin: '15px auto',
+    margin: '10px auto',
     padding: 10,
     width: 256,
   },
@@ -30,53 +40,141 @@ const styles = {
     marginLeft: 'auto',
     marginRight: 'auto',
   },
-  button: {
-    margin: '10px auto',
+  buttonsDiv: {
+    width: 180,
+    margin: '15px auto',
   },
 };
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+      },
+      signupError: true,
+    };
+  }
+
+  static propTypes = {
+    history: React.PropTypes.object,
+  };
+
+  addUser() {
+    firebase.auth().createUserWithEmailAndPassword(this.state.user.email,
+        this.state.user.password).then((user) => {
+          this.saveUser(user);
+          user.updateProfile({
+            displayName: this.state.user.name,
+          }).then(() => {
+            this.props.history.push('/projects');
+          }).catch((error) => {
+            console.log(error);
+          });
+        }).catch((error) => {
+          console.log(error);
+        });
+  }
+
+  handleChange = (event, newValue) => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [event.target.id]: newValue,
+      },
+    });
+
+    if (event.target.id === 'name' && newValue.length > 4) {
+      nameError = false;
+    }
+
+    if ((event.target.id === 'password' && newValue.length > 4 &&
+        newValue === this.state.user.password2) ||
+        (event.target.id === 'password2' && newValue.length > 4 &&
+        newValue === this.state.user.password)) {
+      passwordError = false;
+    }
+
+    if (event.target.id === 'email' && newValue.length > 4) {
+      if (emailRE.test(newValue)) {
+        emailError = false;
+      }
+    }
+
+    if (!nameError && !emailError && !passwordError) {
+      console.log('no err');
+      this.setState({
+        signupError: false,
+      });
+    }
+  };
+
+  saveUser(user) {
+    firebase.database().ref(`users/${user.uid}`).set({
+      name: this.name,
+    });
+  }
 
   render() {
     return <div>
             <Paper style={styles.container}>
                 <div style={ styles.logo }>
-                    <img src="assets/HuxeLogo.png" width="100"/>
+                    <img src="assets/logo.png" width="100" style={{ backgroundColor: '#9E9E9E' }}/>
                     <h1 style={styles.headline}>Signup</h1>
                 </div>
 
                 <div style={styles.inline}>
                     <TextField floatingLabelText="Name"
+                               value={this.state.user.name}
+                               id="name"
+                               onChange={this.handleChange}
                                style={styles.textField}
                     />
                 </div>
 
                 <div style={styles.inline}>
                     <TextField floatingLabelText="Email"
+                               value={this.state.user.email}
+                               id="email"
+                               onChange={this.handleChange}
                                style={styles.textField}
                     />
                 </div>
 
                 <div style={styles.inline}>
                     <TextField floatingLabelText="Password"
+                               value={this.state.user.password}
+                               id="password"
+                               onChange={this.handleChange}
                                type="password"
                     />
                 </div>
 
                 <div style={styles.inline}>
                     <TextField floatingLabelText="Repeat Password"
+                               value={this.state.user.password2}
+                               id="password2"
+                               onChange={this.handleChange}
                                type="password"
                     />
                 </div>
 
-                <RaisedButton label="signup"
-                              primary={true}
-                              style={styles.button}
-                />
+                <div style={ styles.buttonsDiv } >
+                    <RaisedButton label="sign up"
+                                  primary={true}
+                                  disabled={this.state.signupError}
+                                  onTouchTap={() => { this.addUser(); }}
+                    />
 
-                <RaisedButton label="back"
-                              style={styles.button}
-                />
+                    <Link to="/login">
+                        <RaisedButton label="back"
+                        />
+                    </Link>
+                </div>
             </Paper>
         </div>;
   }
