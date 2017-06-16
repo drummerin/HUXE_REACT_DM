36875b976f8d023ecc271934bc0436f282c4be01
Susanne <>
2017-06-16 17:03:08 +0200

    refactoring

--- src/routes/ChatOverview.jsx
+++ src/routes/ChatOverview.jsx
@@ -13,7 +13,7 @@ import CommunicationCallEnd from 'material-ui/svg-icons/communication/call-end';
 import IconButton from 'material-ui/IconButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
-import { green600, grey300 } from 'material-ui/styles/colors';
+import { green600 } from 'material-ui/styles/colors';
 import Call from '../components/Call.jsx';
 import { on, off, send } from '../ws';
 
@@ -39,7 +39,7 @@ const styles = {
     backgroundColor: 'green',
   },
   textFieldPaper: {
-    padding: 15,
+    padding: 10,
     margin: 10,
     width: 350,
   },
@@ -64,11 +64,6 @@ const styles = {
   },
 };
 
-const offerOptions = {
-  offerToReceiveAudio: 1,
-  offerToReceiveVideo: 1,
-};
-
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
@@ -81,14 +76,7 @@ export default class Chat extends React.Component {
 
       curUser: null,
       users: [],
-      localVideo: null,
-      remoteVideo: null,
-      localStream: null,
-      call: false,
-      startTime: null,
     };
-    this.pc1 = null;
-    this.pc2 = null;
   }
 
   static propTypes = {
@@ -101,6 +89,18 @@ export default class Chat extends React.Component {
       this.setState({
         messages: [
           ...this.state.messages,
+          {
+            ...payload,
+            name: from,
+          },
+        ],
+      });
+    });
+
+    on('users', (from, payload) => {
+      this.setState({
+        users: [
+          ...this.state.users,
           {
             ...payload,
             name: from,
@@ -133,11 +133,13 @@ export default class Chat extends React.Component {
   }
 
   componentWillUnmount() {
+    console.log('unmount');
     this.setState({
       name: '',
       nameInput: '',
     });
     off('chat');
+    off('users');
   }
 
   startCall(name) {
@@ -166,207 +168,6 @@ export default class Chat extends React.Component {
     });
   }
 
-/*  start() {
-    console.log('Requesting local stream');
-
-    navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: true,
-    })
-    .then((stream) => {
-      window.stream = stream; // stream available to console
-      this.setState({
-        localStream: stream,
-      });
-      if (window.URL) {
-        this.setState({
-          localVideo: window.URL.createObjectURL(stream),
-        });
-      } else {
-        this.setState({
-          localVideo: stream,
-        });
-      }
-    })
-    .catch((e) => {
-      console.log(`getUserMedia() error: ${e.name}`);
-    });
-  }
-
-  call() {
-    this.setState({
-      call: true,
-    });
-
-    console.log('Starting call');
-    this.setState({
-      startTime: window.performance.now(),
-    });
-
-    const videoTracks = this.state.localStream.getVideoTracks();
-    const audioTracks = this.state.localStream.getAudioTracks();
-    if (videoTracks.length > 0) {
-      console.log(`Using video device: ${videoTracks[0].label}`);
-    }
-    if (audioTracks.length > 0) {
-      console.log(`Using audio device: ${audioTracks[0].label}`);
-    }
-    const servers = null;
-        // Add pc1 to global scope so it's accessible from the browser console
-    window.pc1 = new RTCPeerConnection(servers);
-    this.pc1 = window.pc1;
-    console.log('Created local peer connection object pc1');
-
-    this.pc1.onicecandidate = (e) => {
-      this.onIceCandidate(this.pc1, e);
-    };
-
-        // Add pc2 to global scope so it's accessible from the browser console
-    window.pc2 = new RTCPeerConnection(servers);
-    this.pc2 = window.pc2;
-
-    console.log('Created remote peer connection object pc2');
-    this.pc2.onicecandidate = (e) => {
-      this.onIceCandidate(this.pc2, e);
-    };
-    this.pc1.oniceconnectionstatechange = (e) => {
-      this.onIceStateChange(this.pc1, e);
-    };
-
-    this.pc2.oniceconnectionstatechange = (e) => {
-      this.onIceStateChange(this.pc2, e);
-    };
-
-    this.pc2.onaddStream = (e) => {
-      console.log('on add stream');
-      this.gotRemoteStream(e);
-    };
-    // this.state.pc2.onaddstream = this.gotRemoteStream();
-
-    this.pc1.addStream(this.state.localStream);
-    console.log('Added local stream to pc1');
-
-    console.log('pc1 createOffer start');
-    this.pc1.createOffer(
-            offerOptions,
-        ).then((offer) => {
-          this.onCreateOfferSuccess(offer);
-        }).catch((err) => {
-          this.onCreateSessionDescriptionError(err);
-        });
-  }
-
-  onCreateSessionDescriptionError(error) {
-    console.log(`Failed to create session description: ${error.toString()}`);
-  }
-
-  onCreateOfferSuccess(desc) {
-    console.log('Offer from pc1');
-    console.log('pc1 setLocalDescription start');
-    this.pc1.setLocalDescription(desc).then(() => {
-      this.onSetLocalSuccess(this.pc1);
-    }).catch((error) => {
-      this.onSetSessionDescriptionError(error);
-    });
-    console.log('pc2 setRemoteDescription start');
-    this.pc2.setRemoteDescription(desc).then(() => {
-      this.onSetRemoteSuccess(this.pc2);
-    }).catch((error) => {
-      this.onSetSessionDescriptionError(error);
-    });
-    console.log('pc2 createAnswer start');
-        // Since the 'remote' side has no media stream we need
-        // to pass in the right constraints in order for it to
-        // accept the incoming offer of audio and video.
-    this.pc2.createAnswer().then((answer) => {
-      this.onCreateAnswerSuccess(answer);
-    }).catch((err) => {
-      this.onCreateSessionDescriptionError(err);
-    });
-  }
-
-  onSetLocalSuccess(pc) {
-    console.log(`${this.getName(pc)} setLocalDescription complete`);
-  }
-
-  onSetRemoteSuccess(pc) {
-    console.log(`${this.getName(pc)} setRemoteDescription complete`);
-  }
-
-  onSetSessionDescriptionError(error) {
-    console.log(`Failed to set session description: ${error.toString()}`);
-  }
-
-  gotRemoteStream(e) {
-    console.log(`e ${e}`);
-        // Add remoteStream to global scope so it's accessible from the browser console
-    window.remoteStream = e.stream;
-    this.setState({
-      remoteVideo: this.state.localStream,
-    });
-    console.log('pc2 received remote stream');
-  }
-
-  onCreateAnswerSuccess(desc) {
-    console.log('Answer from pc2:');
-    console.log('pc2 setLocalDescription start');
-    this.pc2.setLocalDescription(desc).then(() => {
-      this.onSetLocalSuccess(this.pc2);
-    }).catch((error) => {
-      this.onSetSessionDescriptionError(error);
-    });
-    console.log('pc1 setRemoteDescription start');
-    this.pc1.setRemoteDescription(desc).then(() => {
-      this.onSetRemoteSuccess(this.pc1);
-    }).catch((error) => {
-      this.onSetSessionDescriptionError(error);
-    });
-  }
-
-  onIceCandidate(pc, event) {
-    if (event.candidate) {
-      this.getOtherPc(pc).addIceCandidate(
-                new RTCIceCandidate(event.candidate),
-            ).then(
-                () => {
-                  this.onAddIceCandidateSuccess(pc);
-                },
-                (err) => {
-                  this.onAddIceCandidateError(pc, err);
-                },
-            );
-      console.log(`${this.getName(pc)} ICE candidate: \n${event.candidate.candidate}`);
-    }
-  }
-
-  onAddIceCandidateSuccess(pc) {
-    console.log(`${this.getName(pc)} addIceCandidate success`);
-  }
-
-  onAddIceCandidateError(pc, error) {
-    console.log(`${this.getName(pc)} failed to add ICE Candidate: ${error.toString()}`);
-  }
-
-  onIceStateChange(pc, event) {
-    if (pc) {
-      console.log(`${this.getName(pc)} ICE state: ${pc.iceConnectionState}`);
-      console.log('ICE state change event: ', event);
-    }
-  }
-
-  getOtherPc(pc) {
-    return (pc === this.pc1) ? this.pc2 : this.pc1;
-  }
-
-  getName(pc) {
-    return (pc === this.pc1) ? 'pc1' : 'pc2';
-  } */
-
-  hangup() {
-    // this.pc1.getVideoTracks()[0].stop();
-    // this.pc1.getAudioTracks()[0].stop();
-  }
-
   nameInput(input) {
     this.setState({
       nameInput: input.replace(/[^\w\s]/gi, '').toLowerCase(),
@@ -385,14 +186,29 @@ export default class Chat extends React.Component {
   }
 
   addUser() {
+    send('users', 'all', {
+      id: this.state.nameInput,
+      name: this.state.nameInput,
+    });
     this.setState({
+      users: [
+        ...this.state.users,
+        {
+          name: this.state.nameInput,
+          id: this.state.nameInput,
+          me: true,
+        },
+      ],
+    });
+
+    /* this.setState({
       users: [
         ...this.state.users,
         {
           name: this.state.nameInput,
         },
       ],
-    });
+    }); */
   }
 
   startChat() {
@@ -412,6 +228,7 @@ export default class Chat extends React.Component {
                     <div>
                         { this.state.users ?
                             this.state.users.map(user => (
+                                !user.me ?
                                     <div key={user.name} style={styles.user}>
                                         <span style={{ marginRight: 10 }}>{user.name}</span>
                                         <IconButton style={styles.iconButton}
@@ -428,6 +245,8 @@ export default class Chat extends React.Component {
                                                     disabled={!this.state.call}>
                                             <CommunicationCallEnd color={green600}/>
                                         </IconButton>
+                                    </div> : <div key={user.name} style={styles.user}>
+                                        <span style={{ marginRight: 10 }}>{user.name}</span>
                                     </div>
                             )) : 'no user'
                         }
@@ -481,16 +300,6 @@ export default class Chat extends React.Component {
                             </form>
                         </Paper>
                     </Paper> : null }
-
-                <video src={this.state.localVideo} autoPlay="autoPlay"/>
-                <br/>
-                <video src={this.state.remoteVideo} autoPlay="autoPlay"/>
-
-                {/* <div>
-                    <button id="startButton">Start</button>
-                    <button id="callButton">Call</button>
-                    <button id="hangupButton">Hang Up</button>
-                </div>*/ }
                 < Call ref = {call => (this.Call = call)}/>
             </Paper>
              :
