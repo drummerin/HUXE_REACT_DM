b4df4e9aa5081ae020910a690b7b86b60f177983
Susanne <>
2017-06-16 15:48:11 +0200

    basic chat

--- src/routes/ChatOverview.jsx
+++ src/routes/ChatOverview.jsx
@@ -12,6 +12,7 @@ import CommunicationCall from 'material-ui/svg-icons/communication/call';
 import CommunicationCallEnd from 'material-ui/svg-icons/communication/call-end';
 import IconButton from 'material-ui/IconButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import { green600, grey300 } from 'material-ui/styles/colors';
 import Call from '../components/Call.jsx';
 import { on, off, send } from '../ws';
@@ -34,6 +35,33 @@ const styles = {
   loginMessage: {
     padding: 15,
   },
+  ownMessages: {
+    backgroundColor: 'green',
+  },
+  textFieldPaper: {
+    padding: 15,
+    margin: 10,
+    width: 350,
+  },
+  messages: {
+    width: 500,
+    padding: 10,
+    margin: 10,
+  },
+  name: {
+    color: 'inherit',
+  },
+  sender: {
+    fontSize: 12,
+    paddingBottom: '5px',
+    color: 'grey',
+    fontStyle: 'italic',
+  },
+  message: {
+    padding: 10,
+    marginBottom: 10,
+    display: 'inline-block',
+  },
 };
 
 const offerOptions = {
@@ -45,8 +73,14 @@ export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      nameInput: '',
+      name: '',
+      messageInput: '',
+      messages: [],
+      startChat: false,
+
       curUser: null,
-      users: null,
+      users: [],
       localVideo: null,
       remoteVideo: null,
       localStream: null,
@@ -62,6 +96,18 @@ export default class Chat extends React.Component {
 
   componentDidMount() {
     this.isUserLoggedIn();
+
+    on('chat', (from, payload) => {
+      this.setState({
+        messages: [
+          ...this.state.messages,
+          {
+            ...payload,
+            name: from,
+          },
+        ],
+      });
+    });
   }
 
   isUserLoggedIn() {
@@ -71,7 +117,6 @@ export default class Chat extends React.Component {
       this.setState({
         curUser: user.displayName,
       });
-      send('join', 'all', this.state.curUser);
     } else {
       this.setState({
         curUser: null,
@@ -88,10 +133,37 @@ export default class Chat extends React.Component {
   }
 
   componentWillUnmount() {
+    this.setState({
+      name: '',
+      nameInput: '',
+    });
+    off('chat');
   }
-    
+
   startCall(name) {
     this.Call.startCall(name);
+  }
+
+  sendMessage(evt) {
+    if (evt) {
+      evt.preventDefault();
+    }
+    const id = Math.round(Math.random() * 100000000000).toString(36);
+    send('chat', 'all', {
+      id,
+      message: this.state.messageInput,
+    });
+    this.setState({
+      messageInput: '',
+      messages: [
+        ...this.state.messages,
+        {
+          message: this.state.messageInput,
+          id,
+          me: true,
+        },
+      ],
+    });
   }
 
 /*  start() {
@@ -295,7 +367,43 @@ export default class Chat extends React.Component {
     // this.pc1.getAudioTracks()[0].stop();
   }
 
+  nameInput(input) {
+    this.setState({
+      nameInput: input.replace(/[^\w\s]/gi, '').toLowerCase(),
+    });
+  }
+
+  setName(evt) {
+    if (evt) {
+      evt.preventDefault();
+    }
+    send('join', 'all', this.state.nameInput);
+    this.setState({
+      name: this.state.nameInput,
+    });
+    this.addUser();
+  }
+
+  addUser() {
+    this.setState({
+      users: [
+        ...this.state.users,
+        {
+          name: this.state.nameInput,
+        },
+      ],
+    });
+  }
+
+  startChat() {
+    this.setState({
+      startChat: true,
+    });
+  }
+
   render() {
+    const ownMessageColor = '#ddfbff';
+
     return <div>
         { this.state.curUser ?
             <Paper style={styles.container}>
@@ -307,7 +415,7 @@ export default class Chat extends React.Component {
                                     <div key={user.name} style={styles.user}>
                                         <span style={{ marginRight: 10 }}>{user.name}</span>
                                         <IconButton style={styles.iconButton}
-                                                    onTouchTap={() => this.start()}>
+                                                    onTouchTap={() => this.startChat()}>
                                             <CommunicationChatBubble color={green600}/>
                                         </IconButton>
                                         <IconButton style={styles.iconButton}
@@ -325,6 +433,55 @@ export default class Chat extends React.Component {
                         }
                     </div>
                 </Paper>
+
+                { !this.state.name ?
+                <Paper style={styles.textFieldPaper} zDepth={2}>
+                <form onSubmit={evt => this.setName(evt)}>
+                    <TextField floatingLabelText="Enter a username"
+                               fullWidth={true}
+                               value={this.state.nameInput}
+                               onChange={(e, v) => this.nameInput(v)}
+                    />
+                    <RaisedButton label="Add User"
+                                  primary={true}
+                                  disabled={!this.state.nameInput}
+                                  onTouchTap={() => this.setName()}/>
+                </form>
+                </Paper> : null }
+                { this.state.startChat ?
+                    <Paper style={styles.messages}>
+                        {this.state.messages.map(chatItem => (
+                            chatItem.me ?
+                                <div key={chatItem.id} style={{ textAlign: 'right' }}>
+                                    <Paper style={{ ...styles.message,
+                                      backgroundColor: ownMessageColor }}>
+                                        <div style={styles.sender}>{this.state.name} </div>
+                                        {chatItem.message}
+                                    </Paper>
+                                </div> :
+                                <div key={chatItem.id} >
+                                    <Paper style={styles.message}>
+                                        <div style={styles.sender}>
+                                            <a href="#"
+                                               style={styles.name}
+                                               onClick={() => this.startCall(chatItem.name)}>
+                                            {chatItem.name}
+                                        </a></div>
+                                        {chatItem.message}
+                                    </Paper>
+                                </div>
+                        ))}
+                        <Paper style={styles.textFieldPaper} zDepth={2}>
+                            <form onSubmit={evt => this.sendMessage(evt)}>
+                                <TextField floatingLabelText="Write something"
+                                           fullWidth={true}
+                                           value={this.state.messageInput}
+                                           onChange={(e, v) =>
+                                               this.setState({ messageInput: v })} />
+                            </form>
+                        </Paper>
+                    </Paper> : null }
+
                 <video src={this.state.localVideo} autoPlay="autoPlay"/>
                 <br/>
                 <video src={this.state.remoteVideo} autoPlay="autoPlay"/>
